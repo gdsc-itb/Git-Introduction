d4320442131a0c2a58c5966e39b6f04fdda3f469
farrel-a <13520110@std.stei.itb.ac.id>
2021-10-02 23:00:38 +0700

    First submission by Farrel Ahmad

---
+++ FarrelAhmad_ITB/Identitas.md
@@ -0,0 +1,6 @@
+# Identitas Farrel Ahmad Tugas GDSC ITB
+
+<br>
+
+## Hasil Output `gdsc.cpp`
+![](https://i.ibb.co/jJHxSHr/Screenshot-from-2021-10-02-22-58-38.png)
---
+++ FarrelAhmad_ITB/gdsc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+
+   std::cout<<"Nama : Farrel Ahmad"<<std::endl;
+   std::cout<<"Jusuran : Teknik Informatika"<<std::endl;
+   std::cout<<"Alasan ingin masuk GDSC : ingin eksplorasi programming lebih jauh"<<std::endl;
+   return 0;
+}

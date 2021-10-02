268742f10c878edc2e4299486d4124a5b8bd8ab9
Addin Munawwar <16521280@mahasiswa.itb.ac.id>
2021-10-02 21:36:28 +0700

    First submission by Addin Munawwar

---
+++ Addin Munawwar Yusuf_Institut Teknologi Bandung/Identitas.md
@@ -0,0 +1,1 @@
+![Identitas.py output](images/identitas.png)
---
+++ Addin Munawwar Yusuf_Institut Teknologi Bandung/Identitas.py
@@ -0,0 +1,8 @@
+#Identitas.py
+print("\nNama : Addin Munawwar Yusuf")
+print("Jurusan : Fakultas STEI")
+print("Alasan ingin masuk GDSC : Pengen belajar web dan app development," 
+" belajar AI dan Machine Learning, serta diarahkan buat ikut lomba-lomba kaya hackaton, competitive programming,"
+" game jam, etc.\n")
+
+

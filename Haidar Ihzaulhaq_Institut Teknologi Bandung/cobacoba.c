49330d91a9952b8b3bf33e3eac27c339aa4478aa
haidarihza <ihzaulhaqidar@gmail.com>
2021-10-03 00:05:14 +0700

    First submission by Haidar Ihzaulhaq

---
+++ Haidar Ihzaulhaq_Institut Teknologi Bandung/cobacoba.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Nama : Haidar Ihzaulhaq\n");
+    printf("Jurusan : Teknik Informatika\n");
+    printf("Alasan ingin masuk GDSC : Ingin menambah relasi dan menambah pengetahuan tentang dunia coding\n");
+}

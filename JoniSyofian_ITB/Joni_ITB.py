776cdf5cd1abd586991b60192ed9aea5da8dc7be
jonisy1406 <joni.syofian@student.itb.ac.id>
2021-10-02 23:26:49 +0700

    First submission by Joni Syofian

---
+++ JoniSyofian_ITB/Joni_ITB.py
@@ -0,0 +1,8 @@
+import markdown
+output = markdown.markdown('''
+  Nama : <Joni Syofian> 
+  Jurusan : <Oseanografi>
+  Alasan ingin masuk GDSC : <ingin menambah ilmu terutama dibidang Data \n\t dan ingin menambah relasi serta mencari pengalaman>
+''')
+
+print(output)

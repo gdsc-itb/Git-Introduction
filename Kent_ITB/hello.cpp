4f219f000f8176094122b3dbb8c9d0b9c5013341
Kent <71258048+kentlius@users.noreply.github.com>
2021-10-02 21:59:34 +0700

    First submission by Kent Liusudarso

---
+++ Kent_ITB/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Nama : Kent Liusudarso\n";
+    cout<<"Jurusan : Teknik Informatika\n";
+    cout<<"Alasan ingin masuk GDSC : tertarik dengan google dan ingin memperluas pengetahuan dibidang teknologi";
+
+    return 0;
+}

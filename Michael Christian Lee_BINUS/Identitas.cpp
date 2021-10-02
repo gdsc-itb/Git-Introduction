020ddbadb46f0b1df1469f66b2bfeed26a28f878
Injoker24 <mcl2411@gmail.com>
2021-10-02 21:31:39 +0700

    First submission by Michael Christian Lee

---
+++ Michael Christian Lee_BINUS/Identitas.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cout << "Nama : Michael Christian Lee" << endl;
+	cout << "Jurusan : Computer Science" << endl;
+	cout << "Alasan ingin masuk GDSC : Ingin melatih dan mendapatkan hard skills yang tidak akan didapatkan di perkuliahan dan menjalin koneksi" << endl;
+}

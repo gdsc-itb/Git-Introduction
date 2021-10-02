6ec4422807542b01fb81c9da09339fce002b2697
edutjie <69960893+edutjie@users.noreply.github.com>
2021-10-02 21:31:55 +0700

    First submission by Eduardus Tjitrahardja

---
+++ EduardusTjitrahardja_UI/Identitas.py
@@ -0,0 +1,4 @@
+nama = "Eduardus Tjitrahardja"
+jurusan = "Ilmu Komputer"
+alasan = "Ingin eksplor lebih tentang Software Engineer"
+print(f"Nama : {nama}\nJurusan : {jurusan}\nAlasan ingin masuk GDSC : {alasan}")

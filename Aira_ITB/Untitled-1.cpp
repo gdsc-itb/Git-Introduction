2fcb4224d63e5162841d3836a36ab8975683f7e7
Aira Thalca <13520101@std.stei.itb.ac.id>
2021-10-02 20:34:52 +0700

    First Submission by Aira

---
+++ Aira_ITB/Untitled-1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cout << "Nama : Aira Thalca Avila Putra" << endl;
+    cout << "Jurusan : Informatika" << endl;
+    cout << "Alasan ingin masuk GDSC : Nambah ilmu" << endl;
+}

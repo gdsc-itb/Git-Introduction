13e7cd783f81058c62cb9f2f5cd925e17921095a
Catherine Angel <catheriner40540@gmail.com>
2021-10-02 22:02:29 +0700

    First submission by Catherine Angel Robin

---
+++ CatherineAngelRobin_UI/perkenalan.py
@@ -0,0 +1,7 @@
+nama = "Catherine Angel Robin"
+jurusan = "Ilmu Komputer"
+alasan = "Saya tertarik dengan rangkaian acara GDSB ITB dan ingin mepelajari Software Engineering lebih dalam."
+
+print("Nama :", nama)
+print("Jurusan :", jurusan)
+print("Alasan ini masuk GDSC :", alasan)

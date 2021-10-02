6d86708bfc935e8909d726c6533efe1a0d7b05a9
Andyana Lilmuttaqina Mafaza <andyanamafaza@gmail.com>
2021-10-02 23:50:11 +0700

    First submission by < Andyana Lilmuttaqina Mafaza >

---
+++ Andyana Lilmuttaqina Mafaza_IPB University/introduction.py
@@ -0,0 +1,3 @@
+print('Nama : Andyana Lilmuttaqina Mafaza')
+print('Jurusan : Ilmu Komputer')
+print('Alasan ingin masuk GDSC : Ingin mengembangkan kemampuan diri dalam bidang pemrograman serta memperluas relasi')

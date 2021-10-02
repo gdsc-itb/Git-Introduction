0c6230186949c20646ebefb1bded730edb81dd9f
keepow <16320003@mahasiswa.itb.ac.id>
2021-10-02 23:55:35 +0700

    First submission by Filan

---
+++ Filan Muhammad Kelvin_ITB/identitas/src/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+cout << "Nama : Filan Muhammad Kelvin\n";
+cout << "Jurusan : Oseanografi\n";
+cout << "Alasan ingin masuk GDSC : Menambah ilmu, relasi dan pengalaman" << endl;
+    return 0;
+}

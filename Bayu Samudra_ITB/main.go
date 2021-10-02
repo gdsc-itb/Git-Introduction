428f61e3b1ef93529d5b4778feab7644cf79f498
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-10-02 20:55:44 +0700

    First submission by Bayu Samudra

---
+++ Bayu Samudra_ITB/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("**********************\n")
+  fmt.Println("Nama : Bayu Samudra")
+  fmt.Println("Jurusan : Teknik Informatika")
+  fmt.Println("Tujuan : Ingin mempelajari lebbih dalam mengenai web development dan mencari relasi baru")
+  fmt.Println("\n***********************")
+}

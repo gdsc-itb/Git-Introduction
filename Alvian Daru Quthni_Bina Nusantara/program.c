581cf34cf5114fe2f9558d9fb390b0a80325d4eb
Alviannn <alvianndq@gmail.com>
2021-10-02 21:33:20 +0700

    First submission by Alvian Daru Quthni

---
+++ Alvian Daru Quthni_Bina Nusantara/program.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Nama : Alvian Daru Quthni\n");
+    printf("Jurusan : Teknik Informatika\n");
+    printf("Alasan ingin masuk GDSC : Ingin meningkatkan pengetahuan, mendapatkan lebih banyak wawasan, dan bertemu dengan orang-orang baru.\n");
+    return 0;
+}

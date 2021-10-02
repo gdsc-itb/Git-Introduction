e0d1e89ca113de88eb148ec909b48eb2b08be4e9
Hendri Antonius <hendriantonius27@gmail.com>
2021-10-02 23:25:18 +0700

    First submission by Hendri Antonius

---
+++ HendriAntonius_UniversitasSurabaya/output.py
@@ -0,0 +1,3 @@
+print("Nama : Hendri Antonius")
+print("Jurusan : Teknik Informatika")
+print("Alasan ingin masuk GDSC : Ingin berkolaborasi bersama sama membuat sebuah projek besar yang bisa berguna buat banyak orang")

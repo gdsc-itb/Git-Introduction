9dadd2565bbc4dd6ae84754dcb9804bfaf232905
patrickamadeus <patrickai0309@gmail.com>
2021-10-02 21:26:39 +0700

    First submission by Patrick

---
+++ Patrick_ITB/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Nama : Patrick Amadeus Irawan\n");
+    printf("Jurusan : Teknik Informatika\n");
+    printf("Alasan ingin masuk GDSC : Menambah pengetahuan teknikal dan non-teknikal terkait SE dan DSA");
+    return 0;
+}

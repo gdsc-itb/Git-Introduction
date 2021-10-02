ca0bf9e6da97c4ee4840e07bb09f5c47798d8dff
Rahmad Setiya Budi <222011623@stis.ac.id>
2021-10-02 23:52:14 +0800

    add screenshot

--- tugasGit.cpp
+++ tugasGit.cpp
@@ -3,4 +3,6 @@ using namespace std;
 
 int main(){
     cout << "Nama : Rahmad Setiya Budi" << endl;
+    cout << "Jurusan : Komputasi Statistik" << endl;
+    cout << "Alasan ingin masuk GDSC : Untuk menambah pengalaman dalam bidang IT";
 }

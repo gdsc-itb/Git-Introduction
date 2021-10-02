d773375b3555a610a73464dac8c21f2c0f11fcaa
Margaretha Olivia <margarethaolivia41@gmail.com>
2021-10-02 22:52:44 +0700

    First submission by Margaretha Olivia Haryono

---
+++ Margaretha Olivia Haryono_Institut Teknologi Bandung/identitas.py
@@ -0,0 +1,3 @@
+print("Nama : Margaretha Olivia Haryono")
+print("Jurusan : STEI - TPB")
+print("Alasan ingin masuk GDSC : untuk meningkatkan skill di bidang programming dan menambah relasi dengan sesama mahasiswa yang memiliki ketertarikan pada dunia teknologi digital.")

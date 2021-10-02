5d8f5f3c923cc487054bf783bea6fcee7ff82344
Talahatu <87047371+Talahatu@users.noreply.github.com>
2021-10-02 22:21:01 +0700

    adding file...

---
+++ MichaelTalahatu_UniversitasSurabaya/day1.php
@@ -0,0 +1,9 @@
+<?php 
+
+echo 'Nama : Michael Talahatu';
+echo '<br>';
+echo 'Jurusan : Teknik Informatika';
+echo '<br>';
+echo 'Alasan ingin masuk GDSC : Ingin menambah wawasan dan koneksi';
+
+?>

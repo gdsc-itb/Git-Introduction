2dbc897c5d2371a0f2ff211e2d71c3fc9817b444
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-10-02 21:02:30 +0700

    First Commit by Hana Fathiyah

---
+++ Hana Fathiyah_ITB/identitas.m
@@ -0,0 +1,6 @@
+% MATLAB
+% GDSC ITB
+
+fprintf("Nama: Hana Fathiyah\n");
+fprintf("Jurusan: Teknik Informatika\n");
+fprintf("Alasan Masuk GDSC: Ingin belajar banyak hal baru dan lebih mengenal dunia IT\n");

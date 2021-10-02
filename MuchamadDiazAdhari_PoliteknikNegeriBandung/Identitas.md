048c09fc1ecf33a8611a508977a4fa9b2a8dda3c
yazwe_ <diazazhari32@gmail.com>
2021-10-02 22:07:10 +0700

    First submission by Muchamad Diaz Adhari

---
+++ MuchamadDiazAdhari_PoliteknikNegeriBandung/Identitas.md
@@ -0,0 +1,1 @@
+![gdscdiaz](https://user-images.githubusercontent.com/65151166/135720855-e4008afb-7059-4c0f-a045-92c8cedf4bb3.jpg)
---
+++ MuchamadDiazAdhari_PoliteknikNegeriBandung/diaz.py
@@ -0,0 +1,8 @@
+Nama = input("Nama : ")
+Jurusan = input("Jurusan :")
+Alasan = input("Alasan ingin masuk GDSC :")
+
+print("-----------")
+print(Nama)
+print(Jurusan)
+print(Alasan)

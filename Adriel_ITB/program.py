ecd7bbab3fefdaf51d4151c76fc7d0f8199d7208
Adriel Timoteo <adriel.timoteo@gmail.com>
2021-10-02 21:10:10 +0700

    First submission by Adriel Timoteo

---
+++ Adriel_ITB/program.py
@@ -0,0 +1,3 @@
+print("Nama : Adriel Timoteo")
+print("Jurusan : Teknik Telekomunikasi")
+print("Alasan ingin masuk GDSC : Ingin belajar mengenai development, khususnya full-stack web development, juga mencari koneksi")

10d3b906ac6e822a03de2587df19811c21489e15
unknown <nabilfarras923@gmail.com>
2021-10-02 22:46:18 +0700

    First submission by Moch Nabil Farras Dhiya

---
+++ Moch Nabil Farras Dhiya_ITB/Identitas.py
@@ -0,0 +1,5 @@
+print("Nama: Moch Nabil Farras Dhiya")
+print("Jurusan: Matematika")
+print("Alasan ingin masuk GDSC: Saya berharap dapat bergabung dengan GDSC ITB karena saya sangat tertarik dengan pemrograman, khususnya di bidang DS, dan ingin meniti karir di bidang tersebut. Dengan bergabung di GDSC, saya berharap dapat memperoleh wawasan yang berarti tentang DS dan mendapatkan gambaran tentang deskripsi pekerjaannya dengan mengerjakan proyek nyata. Saat melakukannya, saya berharap saya juga dapat meningkatkan soft skill saya dan memperluas hubungan dan jaringan saya dengan orang-orang luar biasa yang memiliki minat yang sama dengan saya. Dengan demikian, kita dapat memiliki lingkungan yang mendukung untuk mencapai prestasi luar biasa menuju tujuan kita.")
+
+![""](./pictures/image.png)

da2c693bfff8ef2cc50cb7ef449428f605026115
premasi <rakaguntara10@gmail.com>
2021-10-02 22:15:12 +0700

    First submission by Raka Ryandra Guntara

---
+++ RakaRyandraGuntara_UPI/Main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"==========================================================================================="<<endl;
+    cout<<"Nama    : Raka Ryandra Guntara"<<endl;
+    cout<<"Jurusan : Rekayasa Perangkat Lunak"<<endl;
+    cout<<"Alasan  : Karena saya ingin menambah wawasan dan skill saya mengenai perangkat lunak"<<endl;
+    cout<<"==========================================================================================="<<endl;
+}

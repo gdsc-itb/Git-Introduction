b6bfbdeb0bac6f5a8e586489bf0d02217711c8cb
Felix Fernando <felixzhuang1@gmail.com>
2021-10-02 20:56:59 +0700

    Menambahkan file identitas.py

---
+++ Felix Fernando_ITB/identitas.py
@@ -0,0 +1,10 @@
+name = "Felix Fernando"
+jurusan = "Matematika"
+alasan = "Untuk mendapatkan pengalaman dan pengetahuan di bidang programming, serta untuk memperluas relasi"
+
+print("========================================================================================================")
+print("Nama : %s" %(name))
+print("Jurusan : %s" %(jurusan))
+print("Alasan ingin masuk GDSC :")
+print(alasan)
+print("========================================================================================================")

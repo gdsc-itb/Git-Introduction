5ab9fea4a1bd48d58ff1238b8f2af68cabb7a3f7
arsaizdihar <arsadihar@gmail.como>
2021-10-02 20:58:06 +0700

    First submission by Arsa Izdihar Islam

---
+++ Arsa Izdihar Islam_ITB/main.py
@@ -0,0 +1,3 @@
+print("""Nama\t\t\t: Arsa Izdihar Islam
+Jurusan\t\t\t: STEI ITB
+Alasan ingin masuk GDSC\t: Ingin memperdalam ilmu dan kemampuan dalam programming, baik Software Engineering, Data Science, maupun Machine Learning.""")

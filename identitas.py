411bac991defa5fe11c48e4d5ff4681388a75125
Fitra Nurindra <70202129+fitranurindra@users.noreply.github.com>
2021-10-02 23:42:04 +0700

    Delete identitas.py

--- identitas.py
+++
@@ -1,5 +0,0 @@
-Nama    = "Fitra Nurindra"
-Jurusan = "Teknik Elektro"
-Alasan  = "Karena saya ingin menambah pengalaman serta pengetahuan dalam bidang pemrograman,\nsaya juga ingin menambah relasi dengan orang-orang yang memiliki interest yang sama dengan saya"
-
-print(f"Nama    : {Nama}\nJurusan : {Jurusan}\nAlasan ingin masuk GDSC : {Alasan}")

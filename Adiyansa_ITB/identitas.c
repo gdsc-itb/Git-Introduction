8dcc82a8b4c9911da0845a93feeaf56962186dbb
apwic <16520227@mahasiswa.itb.ac.id>
2021-10-02 21:59:05 +0700

    First submission by Adiyansa

---
+++ Adiyansa_ITB/identitas.c
@@ -0,0 +1,13 @@
+// Nama     : Adiyansa Prasetya Wicaksana   
+// Tanggal  : 02-10-2021
+// Topik    : Git Introduction
+
+#include <stdio.h>
+
+int main() {
+    printf("Nama                : Adiyansa Prasetya Wicaksana\n");
+    printf("Jurusan             : Teknik Informatika\n");
+    printf("Alasan masuk GDSC   : Biar bisa lebih terstruktur dalam explore perihal dunia keinformatikaan\n");
+
+    return 0;
+}

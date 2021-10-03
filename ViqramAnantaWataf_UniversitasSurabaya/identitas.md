943b0351000ad1ded77870c97d7eb082b7134821
viqramwataf <s160420119@Student.ubaya.ac.id>
2021-10-03 21:18:03 +0700

    First submission by Viqram Ananta Wataf

---
+++ ViqramAnantaWataf_UniversitasSurabaya/identitas.md
@@ -0,0 +1,1 @@
+![myImage](identitas.PNG)
---
+++ ViqramAnantaWataf_UniversitasSurabaya/src/myCode.java
@@ -0,0 +1,10 @@
+public class myCode {
+    public static void main(String[] args) {
+        String name = "Nama : Viqram Ananta Wataf";
+        String jurusan = "Jurusan : Teknik Informatika";
+        String alasan = "Alasan ingin masuk GDSC: Saya ingin tahu lebih dalam tentang dunia Information Technology";
+        System.out.println(name);
+        System.out.println(jurusan);
+        System.out.println(alasan);
+    }
+}

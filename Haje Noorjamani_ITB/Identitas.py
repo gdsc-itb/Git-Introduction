aa4748207ac59ac19982428c336cd2df2a5263d9
hajeeee <hajenoorjamani@gmail.com>
2021-10-03 07:35:35 +0700

    First Submission by Haje Noorjamani

---
+++ Haje Noorjamani_ITB/Identitas.py
@@ -0,0 +1,3 @@
+print("Nama: Haje Noorjamani")
+print("Jurusan: Sistem dan Teknologi Informasi")
+print("Alasan ingin masuk GDSC : Saya ingin menambah wawasan mengenai teknologi dan menambah teman yang memiliki ketertarikan yang sama.")

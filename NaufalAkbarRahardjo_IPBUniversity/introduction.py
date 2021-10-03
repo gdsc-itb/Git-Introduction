188e65ab0ccfc5750e12eac3e9ddb99ea30fbd48
rahardjorahardjo <naufalakbrahardjo@apps.ipb.ac.id>
2021-10-03 22:58:29 +0700

    First submission by Naufal Akbar Rahardjo

---
+++ NaufalAkbarRahardjo_IPBUniversity/introduction.py
@@ -0,0 +1,3 @@
+print("Nama : Naufal Akbar Rahardjo")
+print("Jurusan : Ilmu Komputer")
+print("Alasan ingin masuk GDSC : karena saya ingin mempelajari dunia komputer lebih dalam lagi dan juga dapat menggunakan ilmu untuk kebaikan")

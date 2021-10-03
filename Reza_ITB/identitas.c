90a41f1fe22297a1c656f6d5e7d269bd42318e14
Reza <18220012@std.stei.itb.ac.id>
2021-10-03 23:13:25 +0700

    First submission by Reza

---
+++ Reza_ITB/identitas.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Nama   : Fikri Muhammad Fahreza\n");
+    printf("Jurusan: Sistem dan Teknologi Informasi\n");
+    printf("Alasan ingin masuk GDSC: Ingin mengembangkan skill yang telah dimiliki dan dapet byk temen guys\n");
+    
+    return 0;
+}

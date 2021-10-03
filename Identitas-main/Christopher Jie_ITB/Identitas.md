7e3833a54c9b72ab45d6bba0c371234080aebc5f
Christopher Jie <73431564+IONISASI@users.noreply.github.com>
2021-10-03 20:05:39 +0700

    Add files via upload

---
+++ Identitas-main/Christopher Jie_ITB/Identitas.md
@@ -0,0 +1,1 @@
+![Screenshot (1862)](https://user-images.githubusercontent.com/73431564/135754195-a39626d9-deb7-49ba-8451-d2a091183903.png)
---
+++ Identitas-main/Christopher Jie_ITB/identitas.py
@@ -0,0 +1,3 @@
+print ("Nama : Christopher Jie")
+print ("Jurusan : STI")
+print("Alasan ingin masuk GDSC : ingin memperdalam pemahaman tentang teknologi dan mempelajari hal baru terutama di bidang teknologi.")

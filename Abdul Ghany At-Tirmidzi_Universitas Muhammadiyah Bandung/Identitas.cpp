5dff75efc6cf544d88fc30802063d670670015b3
Ghany At-Tirmidzi <aghany08@gmail.com>
2021-10-03 11:12:28 +0700

    Adding Identitas.cpp

---
+++ Abdul Ghany At-Tirmidzi_Universitas Muhammadiyah Bandung/Identitas.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "========================================================================================" << endl;
+    cout << "\n Nama : Abdul Ghany At-Tirmidzi" << endl;
+    cout << " Jurusan : Teknik Informatika" << endl;
+    cout << " Alasan ingin masuk GDSC : Salah satunya karena ingin menambah pengalaman dalam komunitas \n" << endl;
+    cout << "========================================================================================" << endl;
+}

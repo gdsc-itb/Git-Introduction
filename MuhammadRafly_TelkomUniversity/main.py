bf2a4a4c55f27d1610dac2547fa1c9ef89b801c9
Rafly <rraaffllyy22@gmail.com>
2021-10-03 14:40:50 +0700

    Folder Baru

---
+++ MuhammadRafly_TelkomUniversity/main.py
@@ -0,0 +1,2 @@
+nama, jurusan, alasan = "Muhammad Rafly Arjasubrata", "S1 Informatika", "Karena saya ingin belajar web development"
+print(f"Nama : {nama}\nJurusan : {jurusan}\nAlasan ingin masuk GDSC : {alasan}")

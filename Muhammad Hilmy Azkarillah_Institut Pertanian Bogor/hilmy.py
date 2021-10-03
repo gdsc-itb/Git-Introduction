eaa2cb63abf85a872b3832d3c6124517f82908c2
Muhammad Hilmy Azkarillah <31369102+mhazrla@users.noreply.github.com>
2021-10-03 20:29:01 +0700

    First submission by Muhammad Hilmy Azkarillah

---
+++ Muhammad Hilmy Azkarillah_Institut Pertanian Bogor/hilmy.py
@@ -0,0 +1,7 @@
+n = "Muhammad Hilmy Azkarillah"
+j = "Teknik Komputer"
+a = "Ingin belajar bersama dan mencari pengalaman baru bersama teman teman yang lain"
+
+print("Nama                     : ", n)
+print("Jurusan                  : ", j)
+print("Alasan ingin masuk GDSC  : ", a)

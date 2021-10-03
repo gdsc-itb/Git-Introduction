03d640060f36a7b046be1831e8328d794408e290
eugeniusms <eugenius.mario@ui.ac.id>
2021-10-03 16:44:50 +0700

    First submission by Eugenius Mario Situmorang

---
+++ Eugenius Mario Situmorang_Universitas Indonesia/Identitas.py
@@ -0,0 +1,12 @@
+class introduction:
+    
+    def __init__(self, name, major, motivation):
+        self.name = name
+        self.major = major
+        self.motivation = motivation
+        
+me = introduction("Eugenius Mario Situmorang", "Computer Science", "I want to add relationships and learn together to add new experiences with community at GDSC ITB")
+
+print("Name       : "+me.name)
+print("Major      : "+me.major)
+print("Motivation : "+me.motivation)

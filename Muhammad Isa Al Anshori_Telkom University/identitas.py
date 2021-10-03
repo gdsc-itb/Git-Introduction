c614d1edc66d2c5f67d4c57fbbb0652f35cdf48d
M Isa Al Anshori <38964181+misaalanshori@users.noreply.github.com>
2021-10-03 07:36:40 +0700

    First Submission by Muhammad Isa Al Anshori

--- Muhammad Isa Al Anshori_Telkom University/identitas.py
+++ Muhammad Isa Al Anshori_Telkom University/identitas.py
@@ -1,3 +1,5 @@
+print("="*32)
 print("Nama\t\t\t: Muhammad Isa Al Anshori")
 print("Jurusan\t\t\t: S1 Rekayasa Perangkat Lunak")
 print("Alasan ingin Masuk GDSC : ingin lebih mendalami software development, dan menambah teman dan kenalan")
+print("="*32)

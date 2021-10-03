4ddc986b5d2e3871a6c222b79126815e9e59c107
anitadriani <anitaadriani@gmail.com>
2021-10-03 20:40:36 +0700

    First submission by <Devina>

---
+++ Devina_Polban/Devina_Polban.py
@@ -0,0 +1,6 @@
+introduction = {"Nama" : "Devina Lusiana",
+                "Jurusan" : "Teknik Informatika",
+                "Alasan_Ingin_Masuk_GDSC" : "Menambah ilmu baru mengenai web development dan machine learning"}
+
+for key, val in introduction.items():
+    print("%s :%s" % (key,val))

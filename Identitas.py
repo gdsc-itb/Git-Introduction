bd89c7e949ccc6c93198aa66b204ee3682dd1267
Alvaro <91789098+Emilicos@users.noreply.github.com>
2021-10-03 20:32:29 +0700

    Add files via upload

---
+++ Identitas.py
@@ -0,0 +1,8 @@
+print("-"*175)
+print("Halo salam kenal, kalian bisa mengenal lebih banyak tentang aku di bawah ini:")
+print(f"Nama: Alvaro Austin")
+print(f"Jurusan: Ilmu Komputer")
+print(f"Universitas: Universitas Indonesia")
+print(f"Alasan ingin masuk GDSC: Saya ingin menambah pengalaman saya tidak hanya dalam programming namun juga berbagai hal. Menurut saya GDSC ITB adalah tempat yang cocok untuk ini")
+print(f"Game favorit: Dota2")
+print("-"*175)

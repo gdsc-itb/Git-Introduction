982d3dece913deb99cd84bdb6cffae83a383155b
Kurniaman <87873299+Kurniaman@users.noreply.github.com>
2021-10-06 00:49:48 +0700

    Add files via upload

---
+++ Kurniaman Andreas Zega_ITS/index.java
@@ -0,0 +1,11 @@
+public class index {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Kurniaman Andreas Zega");
+        System.out.println("Jurusan : Sistem Informasi");
+        System.out.println("Alasan ingin masuk GDSC : Karena ingin memperbanyak pengalaman, memperdalam ilmu di bidang IT,dan menambah relasi lebih banyak dengan orang yang terbaik di luar sana di luar sana");
+    }
+}
+
+
+

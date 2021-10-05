73bbb578c7d9dc8847d86cbcb9d93b68ea16dfd9
Rachmat Purwa Saputra <rachmatpurwa393@gmail.com>
2021-10-05 20:21:05 +0700

    First submission by Rachmat Purwa Saputra

--- Rachmat Purwa Saputra_Politeknik Negeri Bandung/GDSC_myGitIntro.c
+++ Rachmat Purwa Saputra_Politeknik Negeri Bandung/GDSC_myGitIntro.c
@@ -5,6 +5,6 @@ char namaSaya[] = "Rachmat Purwa Saputra";
 int main() {
     printf("Nama : %s\n", namaSaya);
     printf("Jurusan : %s\n", "Teknik Komputer dan Informatika");
-    printf("Alasan ingin masuk GDSC : %s\n", "Ingin menambah pengetahuan, pengalaman, dan relasi di bidang IT khususnya");
+    printf("Alasan ingin masuk GDSC : %s\n", "Saya ingin menambah pengetahuan, pengalaman, dan relasi di bidang IT khususnya");
     return 0;
 }

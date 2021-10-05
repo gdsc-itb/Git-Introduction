dec804e20ade3b47bb10dd1fc32e2a0d8c0df7c1
darielgaizta <darielgaz@gmail.com>
2021-10-05 13:13:33 +0700

    First Submission by Fatih Darielma Gaizta

---
+++ Fatih Darielma Gaizta_Institut Teknologi Bandung/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Nama : Fatih Darielma Gaizta\n");
+	printf("Jurusan : Sistem dan Teknologi Informasi\n");
+	printf("Alasan ingin masuk GDSC : Ingin belajar lebih jauh untuk menjadi developer atau software engineer\n");
+	return 0;
+}

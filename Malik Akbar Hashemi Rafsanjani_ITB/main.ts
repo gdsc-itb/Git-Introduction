21511b18bcc553a6b409160bae8262289d61e36e
Malik Akbar Hashemi Rafsanjani <pro.malikakbar2357@gmail.com>
2021-10-05 18:01:24 +0700

    First submission by Malik Akbar Hashemi Rafsanjani

---
+++ Malik Akbar Hashemi Rafsanjani_ITB/main.ts
@@ -0,0 +1,7 @@
+const nama : string = "Malik Akbar Hashemi Rafsanjani";
+const jurusan : string = "Teknik Informatika";
+const alasan : string = "Aku bermimpi untuk jadi software engineer yang hebat. Oleh karena itu, aku ingin masuk GDSC, di mana aku berharap aku dapat belajar banyak hal-hal baru, memperluas pengetahuanku, serta mengasah skillku. Tidak hanya itu, aku juga berharap hal ini akan membuka banyak kesempatan-kesempatan karir untukku dan untuk komunitas ^_^";
+
+console.log("Nama : ", nama);
+console.log("Jurusan : ", jurusan);
+console.log("Alasan ingin masuk GDSC : ", alasan);

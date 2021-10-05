8893a1416bbed222d4e94da52fbb3c1366376761
syaghina <16520200@std.stei.itb.ac.id>
2021-10-05 14:42:35 +0700

    First submission by Syaghina Maitsa Gunawan

---
+++ Syaghina_ITB/identitas.py
@@ -0,0 +1,6 @@
+# Hands On
+# 05 Oktober 2021
+
+print("Nama : Syaghina Maitsa Gunawan")
+print("Jurusan : Teknik Telekomunikasi")
+print("Alasan ingin masuk GDSC : Karena ingin meningkatkan wawasan tentang web developer dan data science, serta dapat memperluas relasi.")

ae8d7844e2ffdb2e2155f7586c07fc0485c7867b
Mukhammad Rizki <rizkyRE7@gmail.com>
2021-10-05 15:19:37 +0700

    First submission by Mukhammad Rizki

---
+++ UNAIR_Rizki/Identitas.md
@@ -0,0 +1,1 @@
+<img title="Identitas" src="/Universitas Airlangga_Mukhammad Rizki/GDSC_Identitas_Mukhammad Rizki_UNAIR">
---
+++ UNAIR_Rizki/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Nama : Mukhammad Rizki");
+        System.out.println("Jurusan : S1 - Sistem Informasi");
+        System.out.println("Alasan ingin masuk GDSC : Karena ingin menambah ilmu dan memperluas relasi");
+    }
+}

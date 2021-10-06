6b67294f0af287473ea24ee5bacbef433bd38b43
leonardolim78 <leonardolim178@gmail.com>
2021-10-06 22:19:45 +0700

    First submission by Leonardo

---
+++ Leonardo_Binus University/introduction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "Leonardo";
+    string jurusan = "Computer Science";
+    string alasan = "Karena tertarik dalam pengembangan website dan mobile seperti Flutter dan Android yang dimiliki oleh Google. Selain itu, saya juga ingin meningkatkan keahlian saya dalam dunia pemrograman dan memperluas koneksi saya dengan anggota dari komunitas ini.";
+
+    cout << "Nama                    : " << nama << endl;
+    cout << "Jurusan                 : " << jurusan << endl;
+    cout << "Alasan ingin masuk GDSC : " << alasan << endl;
+
+    return 0;
+}

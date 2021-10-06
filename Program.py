d33c293de689f1ee28e02b83758edb71597b090d
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2021-10-07 00:19:06 +0700

    program python

---
+++ Program.py
@@ -0,0 +1,3 @@
+print("Nama : Angela Livia Arumsari")
+print("Jurusan : TPB STEI")
+print("Alasan ingin masuk GDSC : Ingin belajar dan berkembang di bidang yang diminati sekaligus menambah koneksi.")

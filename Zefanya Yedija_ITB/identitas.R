86c381ce794bfc3b342bd6ae5dc965743c865e6f
ZEFANYAYEDIJA8 <19921011@mahasiswa.itb.ac.id>
2021-10-06 17:32:08 +0700

    First submission by Zefanya Yedija

--- Zefanya Yedija_ITB/identitas.R
+++
@@ -1,7 +0,0 @@
-Nama <- "Zefanya Yedija Hamonangan"
-Jurusan <- "Sekolah Arsitektur, Perencanaan, dan Pengembangan Kebijakan"
-alasanInginMasukGDSC <- "Ingin menambah pengetahuan dan relasi"
-
-Nama
-Jurusan
-alasanInginMasukGDSC

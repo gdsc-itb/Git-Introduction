78b83be803cff3873b6968688f292f690bd50855
Ellena <18120045@mahasiswa.itb.ac.id>
2021-10-06 13:30:42 +0700

    First submission by Ellena Thalia Noveta

---
+++ Ellena Thalia Noveta_ITB/identitas.py
@@ -0,0 +1,10 @@
+#identitas 
+
+nama = 'Ellena Thalia Noveta'
+jurusan = 'Teknik Telekomunikasi'
+alasan = '''ingin memperluas network, belajar hal baru, 
+dan hopefully bikin project yang keren.'''
+
+print('Nama: ' + str(nama))
+print('Jurusan: ' + str(jurusan))
+print('Alasan ingin masuk GSDC: '+ str(alasan))

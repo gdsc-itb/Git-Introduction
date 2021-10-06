eb6cc90547de4e777ce6a6ece40a61230cd79600
jeki-z <hasbidzakiyyah@gmail.com>
2021-10-06 23:36:59 +0700

    first submission by dzakiyyah

---
+++ Dzakiyyah_IPB/saya.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("Nama: Dzakiyyah Hasbi\n");
+    printf("Jurusan: Ilmu Komputer\n");
+    printf("Alasan ingin masuk GDSC: Belajar Web Dev\n");
+    return 0;
+    
+}

bba706945a56c7870366f07b609e2a57c66638e2
azkasena <azkablack@gmail.com>
2021-10-06 18:40:58 +0700

    First commit by Azka Avicenna R

---
+++ Azka Avicenna R_UPNJatim/identitas.py
@@ -0,0 +1,3 @@
+print ("Nama : Azka Avicenna Rasjid")
+print ("Jurusan : Informatika")
+print ("Alasan ingin masuk GDSC : Alasan saya ingin masuk GDSC adalah saya ingin belajar tentang programming baik dari data science hingga web development. Saya juga ingin menambah relasi dengan teman-teman programmer dari luar kampus. ")

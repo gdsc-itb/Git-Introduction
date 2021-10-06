e9c3db8cc93c4d4c9ea02619a63f89e511023cdc
habibshirazy <www.habiburrohman000@gmail.com>
2021-10-06 23:51:49 +0700

    First submission by Habiburrohman

---
+++ Habiburrrohman_ITB/intro.py
@@ -0,0 +1,7 @@
+nama = "Habiburrohman"
+jurusan = "FMIPA"
+alasan = "Saya ingin masuk GDSC ITB untuk mengembangkan kemampuan pemrograman, khususnya bidang Data Science, Artificial Intelligence, dan Web Development. Saya juga ingin membangun relasi dengan teman-teman sesama anggota GDSC."
+
+print("Nama : {}".format(nama))
+print("Jurusan : {}".format(jurusan))
+print("Alasan ingin masuk GDSC : {}".format(alasan))

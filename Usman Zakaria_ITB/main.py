773870c68d11b92ec35e90d05fdae21a5159893a
usmanzak <92038676+usmanzak@users.noreply.github.com>
2021-10-06 21:16:02 +0700

    Create main.py

---
+++ Usman Zakaria_ITB/main.py
@@ -0,0 +1,7 @@
+a = ("Nama : Usman Zakaria") 
+b = ('Jurusan : Teknik Telekomunikasi')
+c = ('Alasan ingin masuk GDSC : Ingin menambah wawasan seputar dunia pemrograman, menadapat banyak relasi, dan juga ilmu baru.')
+
+print(a)
+print(b)
+print(c)

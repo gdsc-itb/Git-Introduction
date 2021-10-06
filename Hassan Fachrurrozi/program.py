d2de493e5ea5c281449a5bc3a91ded0c872240b5
HassanFachh <91823044+HassanFachh@users.noreply.github.com>
2021-10-06 20:42:07 +0700

    Delete Hassan Fachrurrozi directory

--- Hassan Fachrurrozi/program.py
+++
@@ -1,7 +0,0 @@
-x = "Nama : Hassan Fachrurrozi "
-y = "Jurusan : Teknik Telekomunikasi "
-z =  "Alasan ingin masuk GDSC : Ingin mendalami dan meningkatkan kemampuan di bidang teknologi. Tidak hanya itu saya juga ingin mendapatkan banyak pengalaman serta relasi "
-
-print(x)
-print(y)
-print(z)

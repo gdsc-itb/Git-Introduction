236ff9ea2bd5ddf2ccb54b6da94da811b67ea93e
ZEFANYAYEDIJA8 <19921011@mahasiswa.itb.ac.id>
2021-10-06 17:28:39 +0700

    menambahkan folder

---
+++ Zefanya Yedija_ITB/identitas.R
@@ -0,0 +1,7 @@
+Nama <- "Zefanya Yedija Hamonangan"
+Jurusan <- "Sekolah Arsitektur, Perencanaan, dan Pengembangan Kebijakan"
+alasanInginMasukGDSC <- "Ingin menambah pengetahuan dan relasi"
+
+Nama
+Jurusan
+alasanInginMasukGDSC

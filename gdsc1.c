9824e963751554902c39efc2222f439da27c2fd4
LENOVO <ameliahernawan1603@gmail.com>
2021-10-06 16:41:25 +0700

    updating file

--- gdsc1.c
+++ gdsc1.c
@@ -1,1 +1,6 @@
-
+#include <stdio.h>
+int main()
+{
+    printf("Nama : Amelia Hernawan\nJurusan : Ilmu Komputer\nAlasan ingin masuk GDSC : Ingin menambah ilmu, pengalaman, dan teman");
+    return 0;
+}

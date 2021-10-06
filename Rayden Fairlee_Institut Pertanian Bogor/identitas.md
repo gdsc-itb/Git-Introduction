55a9c74ee4f80fd7d3ba18e364fe5076d68a85bb
RaydenF <raydenfairlee@gmail.com>
2021-10-06 15:04:02 +0700

    First submission by Rayden Fairlee

---
+++ Rayden Fairlee_Institut Pertanian Bogor/identitas.md
@@ -0,0 +1,1 @@
+Here is a [link] (file:///C:/Users/user/Pictures/Screenshots/Screenshot%202021-10-06%20145335.png)
---
+++ Rayden Fairlee_Institut Pertanian Bogor/identitas.py
@@ -0,0 +1,3 @@
+print('Nama              : Rayden Fairlee'
+    '\nJurusan           : Ilmu Aktuaria'
+    '\nAlasan masuk GDSC : Ingin menambah ilmu tentang pemrograman dan data science')

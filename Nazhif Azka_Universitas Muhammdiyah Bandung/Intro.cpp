901600427aeaba437cc23b2065e367316b76eebf
nazhif <nazhif245@gmail.com>
2021-10-06 09:00:04 +0700

    Create Folder

---
+++ Nazhif Azka_Universitas Muhammdiyah Bandung/Intro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Nama : Nazhif Azka Fadlillah \n";
+	cout << "Jurusan : Teknik Informatika \n";
+	cout << "Alasan ingin masuk GDSC : Untuh menambah ilmu dan mempelajari skill baru serta menambah kenalan \n";
+
+	return 0;
+}
+

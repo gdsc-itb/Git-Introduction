612678c11a071815b9c12a6d88cc57252020dd68
raisyad <raisyadjullfikar@upi.edu>
2021-10-07 22:13:06 +0700

    First submission by Raisyad Jullfikar

---
+++ Raisyad_Jullfikar_Universitas_Pendidikan_Indonesia/Identitas.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    printf ("Nama : Raisyad Jullfikar\n");
+    printf ("Jurusan : Ilmu Komputer\n");
+    printf ("Alasan ingin masuk GDSC : ingin mempelajari hal baru mengenai alghoritm, menambah relasi serta pengetahuan lebih agar bisa menunjang didunia nyata nantinya\n");
+
+    return 0;
+}

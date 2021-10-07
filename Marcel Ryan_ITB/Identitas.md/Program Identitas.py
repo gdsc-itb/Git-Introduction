76bcc3d96969a6d2a153f96dc44f2c92340cfc74
MarcelRyan <88817627+MarcelRyan@users.noreply.github.com>
2021-10-07 19:43:11 +0700

    Delete Program Identitas.py

--- Marcel Ryan_ITB/Identitas.md/Program Identitas.py
+++
@@ -1,3 +0,0 @@
-print("Nama : Marcel Ryan Antony")
-print("Jurusan : Masih TPB")
-print("Alasan ingin masuk GDSC : Alasan saya ingin masuk GDSC adalah karena saya yakin GDSC adalah tempat terbaik untuk mengembangkan skill programming saya dan juga untuk mencari pengalaman-pengalaman baru beserta teman-teman baru")

aa4cc72800ee9b48ed9f28ff6457a75e22bf0ba6
KenEzekiel <16521040@mahasiswa.itb.ac.id>
2021-10-07 16:44:02 +0700

    First submission by <Kenneth Ezekiel>

---
+++ program.py
@@ -0,0 +1,7 @@
+nama = input("Input nama: ")
+jurusan = input("Input jurusan: ")
+alasan = input("Input alasan: ")
+
+print("Nama :", nama)
+print("Jurusan :", jurusan)
+print("Alasan ingin masuk GDSC :", alasan)

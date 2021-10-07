69bf365f2c1e958c2181c35b1093520c303f7894
audrey-claudia <92109262+audrey-claudia@users.noreply.github.com>
2021-10-08 02:18:32 +0700

    Delete Claudia Audrey Kurniawan_ITB directory

--- Claudia Audrey Kurniawan_ITB/perkenalan.py
+++
@@ -1,4 +0,0 @@
-print("Nama : Claudia Audrey Kurniawan")
-print("Jurusan : Sains dan Teknologi Farmasi")
-print("Alasan ingin masuk GDSC : mau belajar coding karena di SF tidak ada hal-hal seperti itu")
-    

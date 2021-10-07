457a8a715e320b5cf4a3be08d9b805df5a2c4e73
Difa Winanda Adliya <difwinda@gmail.com>
2021-10-08 02:56:14 +0700

    First submission by Difa Winanda Adliya

---
+++ Difa Winanda Adliya_ITB/myname.py
@@ -0,0 +1,4 @@
+print('Nama : Difa Winanda Adliya'
+      '\nJurusan : Matematika'
+      '\nAlasan ingin masuk GDSC : Saya ingin belajar pemrograman lebih lanjut. Saya tertarik dengan '
+      'web development dan machine learning.')

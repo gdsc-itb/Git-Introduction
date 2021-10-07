6146af4e90cff2a217eed99c1988660b2e0b1076
safiq <szarifin20041@gmail.com>
2021-10-08 01:27:45 +0700

    adding Syafiq-Ziyadul-Arifin_Institut-Teknologi-Bandung

---
+++ Syafiq-Ziyadul-Arifin_Institut-Teknologi-Bandung/Identitas.md
@@ -0,0 +1,1 @@
+![output](Identitas.png)
---
+++ Syafiq-Ziyadul-Arifin_Institut-Teknologi-Bandung/gdsc.py
@@ -0,0 +1,4 @@
+print("Nama : Syafiq Ziyadul Arifin",
+      "\nJurusan : STEI",
+      "\nAlasan ingin masuk GDSC : Ingin meningkatkan skill programming dan memperbanyak relasi dengan teman seperjuangan"
+)

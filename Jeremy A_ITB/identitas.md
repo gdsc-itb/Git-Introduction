c59b4c234059e4a5484a9dd15c06562e1a375a97
Jeremyaditya <40911426+Jeremyaditya@users.noreply.github.com>
2021-10-07 18:48:03 +0700

    First Submission by Jeremy A

---
+++ Jeremy A_ITB/identitas.md
@@ -0,0 +1,1 @@
+![image](identitas.png)
---
+++ Jeremy A_ITB/program.hs
@@ -0,0 +1,5 @@
+module Main where 
+    main = do 
+        putStrLn "Nama: Jeremy A"
+        putStrLn "Jurusan: Teknik Telekomunikasi"
+        putStrLn "Alasan Ingin Masuk GDSC : Ingin mengembangkan diri, belajar hal baru, serta memperluas koneksi saya."

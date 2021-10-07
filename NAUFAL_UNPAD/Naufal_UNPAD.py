7b83f0ffbda3f1044288408a8044203b87e02a36
andnaufal <naufalfazzz@gmail.com>
2021-10-07 14:50:06 +0700

    First submission by Naufal

--- NAUFAL_UNPAD/Naufal_UNPAD.py
+++
@@ -1,7 +0,0 @@
-a = "Naufal Andila"
-b = "Bisnis Digital"
-c = "Karena menurut saya tempat ini dapat menjadi sebuah wadah untuk saya mengembangkan skill saya."
-
-print ("Nama: " + a)
-print ("Jurusan: " + b)
-print ("Alasan ingin masuk GDSC: " + c)

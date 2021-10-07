6b96a3f7d175fd7f4ec8357fc56a937230e6bde2
alishalistyaa <alisha.listya@gmail.com>
2021-10-07 21:59:23 +0700

    First Submission by Alisha Listya

---
+++ Program GDSC.py
@@ -0,0 +1,5 @@
+print(''' 
+Nama                    : Alisha Listya
+Jurusan                 : STEI ITB tapi IF AMMINNIN KANG PLIS
+Alasan ingin masuk GDSC : Ingin mempelajari banyak hal baru
+    ''')

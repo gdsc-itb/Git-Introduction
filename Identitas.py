9b19f2d6a2be8d15c1da563a815099d5e49c5ff6
Petani Kode <contoh@petanikode.com>
2021-10-07 22:24:54 +0700

    adding identitas.md

---
+++ Identitas.py
@@ -0,0 +1,3 @@
+print ("Nama                    : <Agung Ade Aryadi>") 
+print ("Jurusan                 : <FTSL ITB>")
+print ("Alasan ingin masuk GDSC : <Ingin belajar hal baru yang sama sekali belum saya ketahui sebelumnya #keepexploring>" )

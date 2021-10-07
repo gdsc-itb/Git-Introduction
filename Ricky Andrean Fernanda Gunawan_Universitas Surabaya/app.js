7f99381c9f639504d1f530787af0442cfac314ce
rickyandrean99 <ricky.ralph09@gmail.com>
2021-10-07 22:22:47 +0700

    First submission by Ricky Andrean Fernanda Gunawan

---
+++ Ricky Andrean Fernanda Gunawan_Universitas Surabaya/app.js
@@ -0,0 +1,3 @@
+console.log('Nama : Ricky Andrean Fernanda Gunawan')
+console.log('Jurusan : Teknik Informatika')
+console.log('Alasan ingin masuk GDSC : Mendalami pengetahuan tentang mobile development dan menambah teman dari kampus lain')

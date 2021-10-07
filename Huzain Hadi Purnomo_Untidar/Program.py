d3ebc3d5c3d0cd75269ec2c9f783f7a780e9f44a
HuzainHadi <92041088+HuzainHadi@users.noreply.github.com>
2021-10-07 22:20:58 +0700

    Add files via upload

---
+++ Huzain Hadi Purnomo_Untidar/Program.py
@@ -0,0 +1,3 @@
+print("Nama : Huzain Hadi Purnomo")
+print("Jurusan : S1 Teknik Elektro")
+print("Alasan ingin masuk GDSC : Menambah wawasan dan relasit terkhusus dalam hal software eng. dan semacamnya. Saya juga tertarik dengan nama Google Developer Students :P")
---
+++ Huzain Hadi Purnomo_Untidar/identitas.md
@@ -0,0 +1,1 @@
+![SS Output](https://user-images.githubusercontent.com/92041088/136410055-1813733e-8a3d-4f07-ad77-d06c140c2ad9.png)

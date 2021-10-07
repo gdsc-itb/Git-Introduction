0bccfe772b80c2b63e5553d731209cac01750e4e
Rava Maulana <ravamaulana14@gmail.com>
2021-10-07 19:21:22 +0700

    First submission by Rava Maulana

---
+++ RavaMaulana_ITB/Identitas.md
@@ -0,0 +1,1 @@
+![my screenshot](C:\Users\Rava Maulana\Documents\Personal\Programming\Programming Practice\tugas_gdsc\Git-Introduction\Screenshot (364).png)
---
+++ tugas_gdsc_identitas.py
@@ -0,0 +1,3 @@
+print("Nama : Rava Maulana")
+print("Jurusan : STEI")
+print("Alasan ingin masuk GDSC : Ingin belajar software development dan data science")

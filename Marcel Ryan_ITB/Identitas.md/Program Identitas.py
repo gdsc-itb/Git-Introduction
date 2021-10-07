fadc9139e5d27ee5a42ff227233cc6a977235663
Marcel Ryan <16521343@mahasiswa.itb.ac.id>
2021-10-07 19:28:34 +0700

    First submission by Marcel Ryan Antony

---
+++ Marcel Ryan_ITB/Identitas.md/Program Identitas.py
@@ -0,0 +1,3 @@
+print("Nama : Marcel Ryan Antony")
+print("Jurusan : Masih TPB")
+print("Alasan ingin masuk GDSC : Alasan saya ingin masuk GDSC adalah karena saya yakin GDSC adalah tempat terbaik untuk mengembangkan skill programming saya dan juga untuk mencari pengalaman-pengalaman baru beserta teman-teman baru")

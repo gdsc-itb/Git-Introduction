6780e3648a0eb5b9da2bd17904726fda1a845421
HuzainHadi <92041088+HuzainHadi@users.noreply.github.com>
2021-10-07 22:21:11 +0700

    Delete Program.py

--- Program.py
+++
@@ -1,3 +0,0 @@
-print("Nama : Huzain Hadi Purnomo")
-print("Jurusan : S1 Teknik Elektro")
-print("Alasan ingin masuk GDSC : Menambah wawasan dan relasit terkhusus dalam hal software eng. dan semacamnya. Saya juga tertarik dengan nama Google Developer Students :P")

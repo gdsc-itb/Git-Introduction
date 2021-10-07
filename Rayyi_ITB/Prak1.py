d9317aaa8096e2cb453fb19c46d45abef3826bde
rayyi <16521370@mahasiswa.itb.ac.id>
2021-10-08 02:00:00 +0700

    First Submission by Rayyi Rahmaid Adha

---
+++ Rayyi_ITB/Prak1.py
@@ -0,0 +1,3 @@
+print("Nama : Rayyi Rahmaid Adha",
+      "\nJurusan : STEI",
+      "\nAlasan ingin masuk GDSC : Ingin meningkatkan skill programming dan memperluas relasi dengan orang-orang yang se-interest")

57cdf1d8db7fe8e245d627e2c08c09da40c31760
Agung Aryadi <89307339+LordHeisenSlayer03@users.noreply.github.com>
2021-10-07 23:00:11 +0700

    Delete Agung Aryadi_ITB directory

--- Agung Aryadi_ITB/Identitas.py
+++
@@ -1,3 +0,0 @@
-print ("Nama                    : <Agung Ade Aryadi>") 
-print ("Jurusan                 : <FTSL ITB>")
-print ("Alasan ingin masuk GDSC : <Ingin belajar hal baru yang sama sekali belum saya ketahui sebelumnya #keepexploring>" )
--- Agung Aryadi_ITB/identitas.md
+++
@@ -1,3 +0,0 @@
-��agung
-
-![Screenshot (899)](https://user-images.githubusercontent.com/89307339/136417159-b6a689c8-f8c1-4ab5-b33b-a170d27c1961.png)

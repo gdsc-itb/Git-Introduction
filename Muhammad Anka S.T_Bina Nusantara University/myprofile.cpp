e23ba12a7bd98ce86637dacd7d66c8ce9cf2034f
AnkaJr <muhammadanka10@gmail.com>
2021-10-08 23:12:24 +0700

    First submission by Muhammad Anka Syahfiera Tasik

---
+++ Muhammad Anka S.T_Bina Nusantara University/myprofile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama : Muhammad Anka Syahfiera Tasik" << endl;
+    cout << "Jurusan : Computer Science" << endl;
+    cout << "Alasan ingin masuk GDSC : Karena ingin menambah pengalaman serta wawasan yang berkualitas dan juga mencari relasi agar dapat membantu saya kedepannya untuk meraih cita-cita saya." << endl;
+    cout << endl;
+    return 0;
+}

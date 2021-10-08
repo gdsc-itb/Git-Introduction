a464a6b4effc3eda15f157d489c24d7e26c50821
fahruladityaaaa <92161304+fahruladityaaaa@users.noreply.github.com>
2021-10-08 20:11:07 +0700

    First submission by Muhammad Fahrul Aditya

---
+++ Muhammad Fahrul Adityaa - ITERA.py
@@ -0,0 +1,3 @@
+print("nama : Muhammad Fahrul Aditya")
+print("jurusan : Sains Data")
+print("alasan : karena saya sangat tertarik dengan web development, data engineering, sains data, dan machine laerning. Dan saya harap saya bisa mengembangkan potensi saya di gdsc itb ")

0b323951f648dd1cdc9d77cb890064a32b3a65d0
brianabyyf <brian.abiyyuf@gmail.com>
2021-10-08 23:02:35 +0700

    First submission by Brian Abiyyu Farras

---
+++ Brian Abiyyu Farras_Bina Nusantara University/myProfile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Nama: Brian Abiyyu Farras\n");
+    printf("Jurusan: Computer Science\n");
+    printf("Alasan: Untuk meningkatkan keterampilan saya yang akan berguna nantinya di masa depan dan membangun hubungan dengan mahasiswa lain.\n");
+
+    return 0;
+}

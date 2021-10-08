57016c83cc5e178baabc9ea3b677f94ec5bcf8e3
rhmn5ty <rahman.satya@gmail.com>
2021-10-08 20:27:28 +0700

    First submission by Rahman Satya

---
+++ Rahman Satya_ITB/Identitas.md
@@ -0,0 +1,5 @@
+# Screenshot Output Program Identitas GDSC ITB
+
+Nama saya Rahman Satya dari ITB dan berikut adalah hasil screenshot output program identitas saya untuk GDSC ITB ini:
+
+![Screenshot](Screenshot_Output_Program_Identitas_Rahman_Satya.png)
---
+++ Rahman Satya_ITB/Identitas.py
@@ -0,0 +1,15 @@
+# Program Identitas GDSC ITB
+# Menuliskan ke layar nama, jurusan, dan alasan ingin masuk GDSC saya
+
+# KAMUS
+# nama, jurusan, alasan_ingin_masuk : str
+
+# ALGORITMA
+
+nama = "Rahman Satya"
+jurusan = "TPB STEI"
+alasan_ingin_masuk = "Ingin mempelajari lebih dalam mengenai dunia pemrograman serta mendapatkan relasi di bidang tersebut."
+
+print("Nama :", nama)
+print("Jurusan :", jurusan)
+print("Alasan ingin masuk GDSC :", alasan_ingin_masuk)

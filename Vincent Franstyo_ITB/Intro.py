9eb03761d5fa49db5e1ee507449a79aa4095d88a
Vincent Franstyo <66989751+SleepYoona@users.noreply.github.com>
2021-10-08 20:38:26 +0700

    Update Intro.py
    First submission by Vincent Franstyo

--- Vincent Franstyo_ITB/Intro.py
+++ Vincent Franstyo_ITB/Intro.py
@@ -2,5 +2,5 @@ print("Nama : Vincent Franstyo\n"
       "Jurusan : STEI \n"
       "Alasan ingin masuk ke GDSC : \nFor an aspiring developer like me, GDSC ITB is the most excellent community.\n"
       "I want to get in touch with other fellow developers to share our thoughts and experiences, \n"
-      "particularly in the field of "
+      "particularly in the path of "
       "Machine Learning, Data Science, and Web Development.")

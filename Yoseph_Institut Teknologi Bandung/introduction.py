ce8f101f8bee34ee7924ded44e6f5c747b6f20a5
yosalx <yoseph141002@gmail.com>
2021-10-08 20:10:47 +0700

    First submission by Yoseph

---
+++ Yoseph_Institut Teknologi Bandung/introduction.py
@@ -0,0 +1,3 @@
+print("Nama                     : Yoseph Alexander Siregar")
+print("Jurusan                  : Teknik Informatika")
+print("Alasan ingin masuk GDSC  : Ingin menambah pengalaman, skill dan pengetahuan seputar IT terurama programming, juga berharap untuk mengembangkan diri dan mempraktekkan pengetahuan yang dimiliki")

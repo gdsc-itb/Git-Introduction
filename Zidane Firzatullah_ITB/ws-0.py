ee46674434498a958ef7872f81673ca8b27f18d9
zidane-itb <89019288+zidane-itb@users.noreply.github.com>
2021-10-09 06:37:27 +0700

    First submission by Zidane Firzatullah

---
+++ Zidane Firzatullah_ITB/ws-0.py
@@ -0,0 +1,9 @@
+ob = {
+    'nama': "Zidane Firzatullah",
+    'jurusan': "TPB(STEI)",
+    'alasan': "Ingin menambah pengetahuan teori dan skill praktikal"
+} 
+
+print("Nama:", ob.get('nama'))
+print("Jurusan:", ob.get('jurusan'))
+print("Alasan ingin masuk GDSC:", ob.get('alasan'))

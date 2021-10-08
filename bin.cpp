4011fd493775afadcb8fae6bf3d9e6c42d8e02e8
unknown <martincyberxpro@gmail.com>
2021-10-09 02:26:35 +0700

    adding Tugas_GDSC.txt

---
+++ bin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+
+    cout<<"Nama             = Martin Ompusunggu"<<endl;
+
+    cout<<"Jurusan          = Teknik Informatikan"<<endl;
+
+    cout<<"Alasan Masuk GDSC= Tentunya ingin belajar,dan mendalami ilmu programing "<<endl;
+    
+    return 0;
+}

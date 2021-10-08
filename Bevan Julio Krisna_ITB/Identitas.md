cbadb81a4f48d0c45c5cfabb555ce43a410a4ee9
unknown <16521060@mahasiswa.itb.ac.id>
2021-10-08 23:14:56 +0700

    First submission by Bevan Julio Krisna

---
+++ Bevan Julio Krisna_ITB/Identitas.md
@@ -0,0 +1,1 @@
+![Screenshot (2)](https://user-images.githubusercontent.com/89295282/136588762-fa631155-e53f-4b86-ab5d-ed431ffe6bc3.png)
---
+++ Bevan Julio Krisna_ITB/Identitas.py
@@ -0,0 +1,3 @@
+print('Nama: Bevan Julio Krisna')
+print('Jurusan: Sekolah Teknik Elektro dan Informatika')
+print('Alasan ingin masuk GDCS: Mengembangkan diri sendiri dan mencari teman belajar')

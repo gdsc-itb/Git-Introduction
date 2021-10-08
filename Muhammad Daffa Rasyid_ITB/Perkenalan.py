7c085e980854b1769fd7fe9bedcdf500d91f0ec8
DaffaITB <16520147@mahasiswa.itb.ac.id>
2021-10-08 23:26:53 +0700

    First submission by Muhammad Daffa Rasyid

--- Muhammad Daffa Rasyid_ITB/Perkenalan.py
+++ Muhammad Daffa Rasyid_ITB/Perkenalan.py
@@ -1,3 +1,3 @@
-print("Nama : <Nama>")
-print("Jurusan : <Jurusan>")
-print("Alasan ingin masuk GDSC : <Alasan>")
+print("Nama : Muhammad Daffa Rasyid")
+print("Jurusan : Teknik Elektro")
+print("Alasan ingin masuk GDSC : Tertarik mendalami coding supaya dapat lebih menguasai IoT dan mencari teman sejenis :D")

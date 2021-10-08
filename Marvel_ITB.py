baf8a3bd467d9107bf92e7ae9c78eda541f31687
Marvel <16521220.@kemahasiswaan.itb.ac.id>
2021-10-09 02:28:56 +0700

    updating

--- Marvel_ITB.py
+++
@@ -1,3 +0,0 @@
-print("Nama : Marvel Subekti")
-print("Jurusan : STEI (Sekolah Teknik Elektro dan Informatika")
-print("Alasan masuk GDSC : mengembangkan minat saya terutama dalam bidang coding , dan bisa menambah lingkaran pertemanan yang sama-sama mau belajar pemrograman") 

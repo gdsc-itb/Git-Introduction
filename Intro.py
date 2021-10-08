e465e2ecc26704fe45aeae7f732e1923707c2659
Vincent Franstyo <sorakiriko28814@gmail.com>
2021-10-08 17:46:28 +0700

    Deleted the file Intro.py from the git repository

--- Intro.py
+++
@@ -1,6 +0,0 @@
-print("Nama : Vincent Franstyo\n"
-      "Jurusan : STEI \n"
-      "Alasan ingin masuk ke GDSC : \nAs an aspiring developer like me, GDSC ITB is the most excellent community.\n"
-      "I want to get in touch with other fellow developers to share our thoughts and experiences, \n"
-      "particularly in the field of "
-      "Machine Learning, Data Science, and Web Development.")

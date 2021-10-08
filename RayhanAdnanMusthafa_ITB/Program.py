e7dbd52495820688c39bf78d29f6096ec8bee1a3
RayhanAdnan <radnanmusthafa15@gmail.com>
2021-10-08 11:09:36 +0700

    First submission by Rayhan Adnan Musthafa

---
+++ RayhanAdnanMusthafa_ITB/Program.py
@@ -0,0 +1,3 @@
+print("Nama : Rayhan Adnan Musthafa")
+print("Jurusan : Aktuaria")
+print("Alasan ingin masuk GDSC : Ingin belajar tentang programming dan software development dan ingin mencoba menyelesaikan masalah dunia nyata menggunakan teknologi yang Saya buat")

b6d0575e738beabda4150c2829cec95086befb0c
Frederico Akira <fredericoakira007@gmail.com>
2021-10-08 17:15:40 +0700

    First Submission by Frederico Akira

---
+++ FredericoAkira_BINUS/TugasA.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	
+	printf("Nama : Frederico Akira\n");
+	printf("Jurusan : Computer Science\n");
+	printf("Alasan Masuk GDSC : Pengen Mengembangkan Skill Programming\n");
+	
+	return 0;
+}

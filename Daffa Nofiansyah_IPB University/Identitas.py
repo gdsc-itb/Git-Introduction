2597d839e0db7f26428a9463590fae26b13b052a
unknown <daffanofiansyah131@gmail.com>
2021-10-08 18:58:10 +0700

    First submission by Daffa Nofiansyah

---
+++ Daffa Nofiansyah_IPB University/Identitas.py
@@ -0,0 +1,3 @@
+print('Nama : Daffa Nofiansyah')
+print('Jurusan : Ilmu Komputer')
+print('Alasan ingin masuk GDSC : Ingin mengembangkan skill dan pengetahuan di bidang IT serta menambah relasi dengan orang lain yang memiliki minat yang sama')

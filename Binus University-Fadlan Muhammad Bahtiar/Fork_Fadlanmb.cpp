e2f67dc4263c6baa11b2a435a6a26f105b1e5ba2
Fadlan Muhammad Bahtiar <fadlanmuhammadbahtiar@Fadlans-MacBook-Air.local>
2021-10-08 19:01:43 +0700

    my First submission Fadlan Muhammad Bahtiar

---
+++ Binus University-Fadlan Muhammad Bahtiar/Fork_Fadlanmb.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Nama                    : Fadlan Muhammad Bahtiar\n";
+    cout << "Jurusan                 : Computer Science \n";
+    cout << "Alasan ingin masuk GDSC : Ingin menambah wawasan ilmu di bidang IT dan mendapatkan teman baru agar bisa sharing";
+}

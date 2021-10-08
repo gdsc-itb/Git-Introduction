073b45cd47e2b3eacae440b0f45997b57e70d4b8
Christina Wijaya <16521224@mahasiswa.itb.ac.id>
2021-10-08 16:55:21 +0700

    deleting Tugas1_GDSC.py from Git-Introduction folder

--- Tugas1_GDSC.py
+++
@@ -1,6 +0,0 @@
-print("Nama : Christina Wijaya")
-print("Jurusan : TPB-STEI")
-print("Alasan ingin masuk GDSC : Saya ingin belajar banyak hal baru, terutama tentang pemrograman,\n"
-"berhubung saya juga mahasiswa STEI ITB jadi hal tersebut sangat berkaitan dengan bidang studi\n"
-"yang saya tempuh sekarang. Saya juga ingin mengenal orang-orang hebat dari GDSC ITB.\n"
-"Dan harapannya, saya juga bisa berkembang menjadi pribadi yang jauh lebih baik dari saat ini.")

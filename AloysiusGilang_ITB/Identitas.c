b7055add43652b5af1da848c28ab713cf2019ac4
Aloysius Gilang <gilangpramdya84@gmail.com>
2021-10-08 21:16:19 +0700

    First submission by Aloysius Gilang

---
+++ AloysiusGilang_ITB/Identitas.c
@@ -0,0 +1,12 @@
+/* File :Identitas.c */
+/* Deskripsi : menampilkan identitas */
+
+#include <stdio.h>
+
+int main()
+{
+   printf("Nama : Aloysius Gilang Pramudya\n");
+   printf("Jurusan : Informatika\n");
+   printf("Alasan ingin masuk GDSC : Saya ingin mengasah skill, menambah pengetahuan saya di bidang web development dan menambah relasi di komunitas ini");
+  return 0;
+}
---
+++ AloysiusGilang_ITB/Identitas.md
@@ -0,0 +1,1 @@
+![screenshot](/Users/aloysiusgilang/Documents/ss.png)

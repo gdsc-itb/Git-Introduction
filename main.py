b4cfa41aec23d3dbbcafd4a545f14b899d85471b
Aulia Nadhirah <Aulianybk123@gmail.com>
2021-10-08 10:40:59 +0700

    Add image

---
+++ main.py
@@ -0,0 +1,3 @@
+print('Nama : Aulia Nadhirah Yamsin Badrulkamal')
+print('NIM : 16521204')
+print('Alasan masuk GSDC : saya ingin mengembangkan skill saya dalam \n software engineering dan juga belajar untuk memanfaatkan aplikasi \n untuk menciptakan inovasi yang bisa membawa perubahan')

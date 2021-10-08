3b8a6d29bb3f64fe998b4485627ac6a7d04907bc
khoilifars <khoilifars@gmail.com>
2021-10-08 18:44:25 +0700

    Adding Folder Khoilifa Rahma Syifani_UGM

---
+++ Khoilifa Rahma Syifani_UGM/Khoilifa Rahma Syifani_UGM.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+	cout<<"\nNama\t\t\t: Khoilifa Rahma Syifani\n";
+	cout<<"Jurusan\t\t\t: Teknologi Rekayasa Instrumentasi dan Kontrol\n";
+	cout<<"Alasan ingin masuk GDSC\t: Karena GDSC UGM oprecnya lama, saya memilih yang lebih cepat,\n\t\t\tyaitu GDSC ITB, karena sesuatu yang lebih cepat seringkali lebih baik, terlebih untuk belajar.\n";
+	cout<<"\t\t\tAlasan lain, tentunya, saya ingin belajar mengenai web developing, machine learning, \n\t\t\tsains data, dan hal-hal yang berhubungan dengan teknologi lainnya.\n";
+	cout<<"\t\t\tSerta menambah relasi karena jangkauan member GDSC ITB sangatlah luas.";
+	cout<<"\n\nMakasi Kakak-kakak GDSC ITB <3";
+	return 0;}
+
+

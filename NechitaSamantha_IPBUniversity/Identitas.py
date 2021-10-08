4771c0dc4e4f161d613e0d5f88dfa1571dfeb320
nechitast <85380886+nechitast@users.noreply.github.com>
2021-10-08 12:36:59 +0700

    First submission by Nechita Samantha

---
+++ NechitaSamantha_IPBUniversity/Identitas.py
@@ -0,0 +1,3 @@
+print("Nama : Nechita Samantha")
+print("Jurusan : Ilmu Komputer")
+print("Alasan ingin masuk GDSC : ingin menambah relasi dengan teman-teman yang tertarik di dalam bidang teknologi, memperluas dan memperdalam ilmu tentang teknologi")

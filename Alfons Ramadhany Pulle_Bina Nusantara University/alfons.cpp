673d488133f307bba8df48388dfa9ea5ce1e1408
AlfonsRamadhany <alfons.ramadhany22@gmail.com>
2021-10-09 00:02:58 +0700

    First submission by Alfons Ramadhany Pulle

---
+++ Alfons Ramadhany Pulle_Bina Nusantara University/alfons.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout <<	"Nama 			: Alfons Ramadhany Pulle" <<endl;
+	cout << "Jurusan 		: Computer Science" <<endl;
+	cout << "Alasan Ingin masuk GSDC : Ingin belajar lebih disini, menambah skill, mengikuti seminar-seminar, mengikuti kegiatan- kegiatan GDSC dan tentunya mendapatkan teman dan koneksi baru disini" <<endl;
+
+    return 0;
+}

b1db25b264b29e765d271f7f83accdf5ccddb007
M. Fahkry Malta <muhammadfahkry@gmail.com>
2021-10-08 23:32:36 +0700

    First submission by Muhammad Fahkry Malta

---
+++ Muhammad Fahkry Malta_Institut Teknologi Bandung/identitas.py
@@ -0,0 +1,6 @@
+nama = "Muhammad Fahkry Malta"
+jurusan = "Teknik Informatika"
+alasan = "Ingin menambah ilmu dan lebih memahami produk-produk Google serta menambah relasi"
+print("Nama : ", nama)
+print("Jurusan : ", jurusan)
+print("Alasan ingin masuk GDSC : ",alasan)

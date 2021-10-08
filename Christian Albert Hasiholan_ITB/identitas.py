6accf6fca62b5fa59b35b0b1b6a79e8f45aef6ad
Chris Albert <christiangultom5@gmail.com>
2021-10-08 22:49:06 +0700

    First submission by Christian Albert Hasiholan

---
+++ Christian Albert Hasiholan_ITB/identitas.py
@@ -0,0 +1,3 @@
+print("Nama : Christian Albert Hasiholan")
+print("Jurusan : TPB STEI")
+print("Alasan ingin masuk GDSC: Karena ingin melatih serta memperdalam skill, juga mempelajari berbagai hal baru, khususnya yang berkaitan dengan programming dan teknologi. Juga menambah koneksi dengan orang-orang yang baru saya temui di dalam komunitas ini, yang mungkin saja akan berkolaborasi di masa depan.")

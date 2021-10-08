563053d9eec79044f9e6522ee9b02194f54cd41d
althaftaqiyyah <91841825+althaftaqiyyah@users.noreply.github.com>
2021-10-08 20:10:00 +0700

    Add files via upload

---
+++ Althaf Nawadir Taqiyyah_IPB/Identitas.py
@@ -0,0 +1,4 @@
+print("Nama: Althaf Nawadir Taqiyyah")
+print("Jurusan: Ilmu Komputer")
+print("Alasan ingin masuk GDSC: Saya ingin mengembangkan kemampuan saya di bidang IT, khususnya pada web development. Selain itu, saya juga ingin\n"
+      "                         menambah pengalaman dan memperluas relasi dengan orang-orang yang juga tertarik dan belajar di bidang ini.")

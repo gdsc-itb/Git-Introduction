6f83450c1b65eab5af8d58b69e50d25ed2d5083b
Aziz8860 <muhaariq@gmail.com>
2021-10-08 23:02:59 +0700

    First submission by M Ariq Aziz

---
+++ M Ariq Aziz_IPB/Identitas.md
@@ -0,0 +1,2 @@
+
+![UntitledGDSC](https://user-images.githubusercontent.com/57658403/136587714-3fb79baa-d8da-4c4d-b59f-c8429ff7f450.png)
---
+++ M Ariq Aziz_IPB/M Ariq Aziz_IPB.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Nama: Muhammad Ariq Aziz\n");
+    printf("Jurusan: Ilmu Komputer\n");
+    printf("Alasan ingin masuk GDSC: Mencari ilmu programming dan memperluas relasi\n");
+    
+    return 0;
+}

6aa0b0f9be3a738f4a8eb8c1c3470629341b285d
melvinkent <16521247@mahasiswa.itb.ac.id>
2021-10-08 17:18:52 +0700

    upoading files

---
+++ identitas.py
@@ -0,0 +1,3 @@
+print("Nama : Melvin Kent Jonathan")
+print("Jurusan : TPB STEI")
+print("Alasan ingin masuk GDSC : Saya inggin menjadi seorang developer di masa depan. Saya percaya bahwa GDSC merupakan wadah yang tepat bagi saya untuk mewujudkan mimpi tersebut.")

3a4e429351f5f5d32f1fd3ed02f6d8b4363468e7
Kenny BNP <kenny_bnp@TUNASs-MacBook-Air.local>
2021-10-09 12:45:19 +0700

    First submission by Kenny Benaya Nathan

---
+++ Kenny Benaya Nathan_ITB/identitas.md
@@ -0,0 +1,4 @@
+<!-- Bukti foto output git introduction Kenny Benaya Nathan_ITB -->
+
+![](output_identitas.png)
+
---
+++ Kenny Benaya Nathan_ITB/identitas.py
@@ -0,0 +1,3 @@
+print('Nama     : Kenny Benaya Nathan')
+print('Jurusan  : TPB STEI ITB')
+print('Alasan ingin masuk GDSC : Belajar mengenai dunia developer dan tech industry sekaligus mencoba menjalin relasi')

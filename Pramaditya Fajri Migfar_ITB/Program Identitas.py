11e06ae89e042fb5336087cac269e5c1898599fe
fajrimgfr <pfajrimigfar@gmail.com>
2021-10-09 13:12:34 +0700

    Pengumpulan tugas

---
+++ Pramaditya Fajri Migfar_ITB/Program Identitas.py
@@ -0,0 +1,4 @@
+print("Nama : Pramaditya Fajri Migfar")
+print("Jurusan : Sekolah Teknik Elektro dan Informatika")
+print("Alasan ingin masuk GDSC : Mencari ilmu baru dan meningkatkan ilmu yang sudah ada dalam lingkup pemrogaman dan menambah relasi dan koneksi dengan orang yang memiliki minat yang sama")
+input("Ketik enter untuk close")

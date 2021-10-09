c725c1a21af0100b41bc70dce712f75949cf6c73
naon-ieu <70198929+naon-ieu@users.noreply.github.com>
2021-10-09 09:43:15 +0700

    First submission by Fawwaz Anugrah Wiradhika Dharmasatya

---
+++ Fawwaz Anugrah Wiradhika Dharmasatya_ITB/Identitas.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Nama : Fawwaz Anugrah Wiradhika Dharmasatya\n");
+    printf("Jurusan : Teknik Informatika\n");
+    printf("Alasan ingin masuk GDSC : Ingin mengembangkan diri dan memperluas pengetahuan terutama dibidang web development dan mobile development\n");  
+    return 0;
+}

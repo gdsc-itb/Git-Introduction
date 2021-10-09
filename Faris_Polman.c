fb0103b161681b462f35b9214192c0342d08e54e
Farisrz1 <namaguefaris13@gmail.com>
2021-10-09 13:05:20 +0700

    First submission by Faris RZ

---
+++ Faris_Polman.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+using namespace std;
+
+int main (){
+    printf ("Nama : Faris Rasyid Zhafir");
+    printf ("Jurusan : Automation Engineering");
+    printf ("Alasan ingin masuk GDSC : Untuk membentuk individu dan masyarakat yang mandiri dan berdaya bagi sesama");
+    return 0;
+}
---
+++ Faris_Polman.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+using namespace std;
+
+int main (){
+    printf ("Nama : Faris Rasyid Zhafir\n");
+    printf ("Jurusan : Automation Engineering\n");
+    printf ("Alasan ingin masuk GDSC : Untuk membentuk individu dan masyarakat yang mandiri\ndan berdaya bagi sesama");
+    return 0;
+}

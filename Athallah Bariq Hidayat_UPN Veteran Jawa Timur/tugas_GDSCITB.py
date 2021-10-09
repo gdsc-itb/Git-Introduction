3b777d7c9fc33a4ab7fa6730d92e43e95d6b7932
Atha <71437726+Sandwich44@users.noreply.github.com>
2021-10-09 10:53:46 +0700

    Create tugas_GDSCITB.py

---
+++ Athallah Bariq Hidayat_UPN Veteran Jawa Timur/tugas_GDSCITB.py
@@ -0,0 +1,3 @@
+print("Nama : Athallah Bariq Hidayat")
+print("Jurusan : Sistem Informasi")
+print("Alasan ingin masuk GDSC : Ingin mempelajari dan mendapatkan ilmu baru, khususnya dalam tech industry")

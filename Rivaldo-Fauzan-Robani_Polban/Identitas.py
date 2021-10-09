0c1ea85eaba8b8c420c2a976b90e5334b79b0c62
rivaldofr <rivaldofr03@gmail.com>
2021-10-09 14:15:34 +0700

    First submission by Rivaldo Fauzan Robani

---
+++ Rivaldo-Fauzan-Robani_Polban/Identitas.py
@@ -0,0 +1,3 @@
+print(" Nama : Rivaldo Fauzan Robani ")
+print(" Jurusan : Teknik Informatika")
+print(" Alasan ingin masuk GDSC : Karena ingin mengembangkan ilmu di bidang pemprograman, memperbanyak relasi dan pengalaman serta berkontribusi sebaik baik mungkin dalam komunitas ini")

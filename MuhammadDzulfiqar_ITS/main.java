e9f1d2bd29f45c49c2691dca9170c7fb497123d5
Your Name <muhammaddzulfiqar03@gmail.com>
2021-10-09 10:44:46 +0700

    adding MuhammadDzulfiqar_ITS.folder

---
+++ MuhammadDzulfiqar_ITS/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+  
+    public static void main(String[] args) {
+     String nama,jurusan,alasaninginmasukGDSC;
+  nama = "Muhammad Dzulfiqar";
+  jurusan = "Fisika";
+  alasaninginmasukGDSC = "Alasan Saya ingin masuk menjadi anggota GDSC karena saya ingin mengasah skill Saya \n  dan Saya ingin menyaring banyak relasi dan networking dan ingin berkontribusi dalam dunia developer";
+  
+  System.out.println("Nama :" +" "+ nama);
+    System.out.println("Jurusan :" +" "+ jurusan);
+    System.out.println("Alasan Ingin Masuk GDSC :" +" "+ alasaninginmasukGDSC);
+}
+}
+

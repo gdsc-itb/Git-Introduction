1e1d297765d58f14060e474a0ed193f3904d48ee
alkali007 <ridtowa@gmail.com>
2021-10-09 16:37:41 +0700

    First submission by <Ridha Fatony I.>

---
+++ RidhaFatony_ITB/identitas.py
@@ -0,0 +1,3 @@
+print("Nama                     : Ridha Fatony Iswahyudi")
+print("Jurusan                  : Meteorologi")
+print("Alasan ingin masuk GDSC  : Suka dengan teknologi sejak kecil dan ingin mengembangkan skill dan hobi untuk karier kedepannya dan menambah relasi")

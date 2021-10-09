2594d6e23e8d5155b3f8168d63fe0d706711ee8b
Stanley3477 <13520028@std.stei.itb.ac.id>
2021-10-09 13:51:42 +0700

    First submission by Timothy Stanley Setiawan

---
+++ introduction.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Nama : Timothy Stanley Setiawan\n");
+    printf("Jurusan : Teknik Informatika\n");
+    printf("Alasan ingin masuk GDSC : karena tertarik dengan perkembakagan teknologi dan produk-produk teknologi di google\n");
+    return 0;
+}

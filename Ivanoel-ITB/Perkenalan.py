8cc0fd604a23cdd459d5472e1304b02aacad5db9
Ivanss17 <16521006@mahasiswa.itb.ac.id>
2021-10-09 07:37:45 +0700

    adding Ivanoel-ITB

---
+++ Ivanoel-ITB/Perkenalan.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Nama: Ivanoel Rhema Christama Adhiprama")
+print("Jurusan: STEI")
+print("Alasan ingin masuk GDSC: Supaya dapat mengenal dunia pemrogaman dan developer lebih dalam")
+
+
+# In[ ]:
+
+
+
+
---
+++ Ivanoel-ITB/identitas.md
@@ -0,0 +1,1 @@
+![alt text](./Perkenalan.png)

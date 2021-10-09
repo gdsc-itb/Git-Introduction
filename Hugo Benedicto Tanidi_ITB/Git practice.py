2690ee04da83185e3f57c346537610af6721bfac
Hugo Benedicto Tanidi <88760617+kingdicto@users.noreply.github.com>
2021-10-09 23:21:02 +0700

    Add files via upload

---
+++ Hugo Benedicto Tanidi_ITB/Git practice.py
@@ -0,0 +1,7 @@
+x = input("Masukkan  nama Anda : ")
+y = input("Masukkan jurusan Anda : ")
+z = input("Masukkan alasan mengapa Anda ingin masuk GDSC : ")
+
+print("Nama : "+ str(x))
+print("Jurusan : "+ str(y))
+print("Alasan ingin masuk GDSC : "+str(z))
---
+++ Hugo Benedicto Tanidi_ITB/Identitas.md
@@ -0,0 +1,1 @@
+<a href="https://ibb.co/NV2SxwC"><img src="https://i.ibb.co/4mRdWBt/Screenshot-48.png" alt="Screenshot-48" border="0"></a>

07cee4bf9845a4a352b740ca476eeccb24575980
andrrsennz <andrrsenn@gmail.com>
2021-10-09 22:27:54 +0700

    First submission by Andersen

---
+++ Andersen_ITB/Identitas_Diri.py
@@ -0,0 +1,3 @@
+print("Nama : Andersen")
+print ("Jurusan : Sekolah Teknik Elektro dan Informatika")
+print ("Alasan ingin masuk GDSC : Karena saya ingin belajar lebih jauh dan mencari lebih banyak relasi di dunia teknologi.")

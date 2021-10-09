dd06dcb003a9db21545a023efb032c3f4c95598c
mnyasin26 <forsvive22@gmail.com>
2021-10-09 12:35:52 +0700

    First submission by Muhamad Nur Yasin Amadudin

---
+++ Muhamad Nur Yasin Amadudin_Universitas Pendidikan Indonesia/Identitas.md
@@ -0,0 +1,3 @@
+# Screenshot program
+
+![@mnyasin26](https://i.ibb.co/qsXxq3r/Screenshot-2021-10-09-114209.png)
---
+++ Muhamad Nur Yasin Amadudin_Universitas Pendidikan Indonesia/hellogdscitb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    char nama[1000] = "Muhamad Nur Yasin Amadudin";
+    char jurusan[1000] = "Ilmu Komputer";
+    char alasan_msk_gdscitb[1000] = "I would like to join this community based \non my passion for the world of \ntechnology and some basic skills that I \nhave like problem-solving at technical-related \nthings which are enormous in me and also I do have a good \npractice to operate a computer which can help me \nbe a considerable self-learners.";
+
+    printf("Nama : %s\n", nama);
+    printf("Jurusan : %s\n", jurusan);
+    printf("Alasan ingin masuk GDSC : %s\n", alasan_msk_gdscitb);
+
+    return 0;
+}

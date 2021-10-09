84d8db87e9e9f973bfcd18e10ceebe9ba85acdd2
roastland <tfpruslanali@gmail.com>
2021-10-09 11:32:02 +0700

    First submission by Taufan Fajarama P.R.

---
+++ TaufanFajaramaPR_ITB/GDSCGit.java
@@ -0,0 +1,9 @@
+package TaufanFajaramaPR_ITB;
+
+public class GDSCGit {
+    public static void main(String[] args) {
+        System.out.println("Nama : Taufan Fajarama Putrawansyah Ruslanali");
+        System.out.println("Jurusan : Teknik Informatika");
+        System.out.println("Alasan ingin masuk GDSC : Untuk mengembangkan dan mengeksplor minat di dunia teknologi");
+    }
+}

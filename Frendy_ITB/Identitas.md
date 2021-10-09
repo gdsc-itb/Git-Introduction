9ba66893c5204babffbfb1f962529465c0367dcb
frendysanusi05 <frendyvivo5@gmail.com>
2021-10-09 11:28:35 +0700

    First submission by Frendy Sanusi

---
+++ Frendy_ITB/Identitas.md
@@ -0,0 +1,2 @@
+Identitas 
+![](abc.png)
---
+++ Frendy_ITB/Identitas.py
@@ -0,0 +1,7 @@
+nama = 'Frendy Sanusi'
+jurusan = 'Tahap Persiapan Bersama Sekolah Teknik Elektro dan Informatika'
+alasan = 'Ingin menambah pengetahuan di bidang teknologi berbasis Google serta relasi'
+
+print(f'Nama : {nama}')
+print(f'Jurusan : {jurusan}')
+print(f'Alasan ingin masuk GDSC : {alasan}')

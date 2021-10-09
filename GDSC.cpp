75d65f15a8b79aaeb6759fce12a2eae3a9df890f
gaizkiaadeline <90249058+gaizkiaadeline@users.noreply.github.com>
2021-10-09 13:05:27 +0700

    Add files via upload

---
+++ GDSC.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	
+	printf("Nama : Gaizkia Adeline Atmaka\n");
+	printf("Jurusan : Computer Science and Mathematics\n");
+	printf("Alasan ingin masuk GDSC : To improve my programming skills which will be useful in my future career and grow with other people as developer.\n");
+	
+	return 0;
+}

d30323c56ca4e1af4e585440ccbced9eb8408316
Muhamad Fikri <fikri23134@gmail.com>
2021-10-09 12:12:29 +0700

    First submission by Muhamad Fikri

---
+++ Muhamad Fikri_ITB/identitas.py
@@ -0,0 +1,3 @@
+print('Nama : Muhamad Fikri')
+print('Jurusan : STEI - TPB')
+print('Alasan ingin masuk GDSC : Untuk belajar dan mengembangkan skill saya di bidang teknologi, serta mencari komunitas yang mendukung hal tersebut.')

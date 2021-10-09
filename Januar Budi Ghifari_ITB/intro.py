8701d098985cf9113ced798132ac776c0346bd82
buditato <datajanoe@gmail.com>
2021-10-09 07:09:05 +0700

    First submission by Januar Budi Ghifari

---
+++ Januar Budi Ghifari_ITB/intro.py
@@ -0,0 +1,3 @@
+print("Nama                     :   Januar Budi Ghifari")
+print("Jurusan                  :   Teknik Informatika")
+print("Alasan ingin masuk GDSC  :   Ingin belajar sekaligus explore di bidang software development.")

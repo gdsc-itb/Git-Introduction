00a9878f178d2773966174bc32c6e9a50476cf13
fadhildone <fadhildone@gmail.com>
2021-10-14 07:03:09 +0700

    First submission by Rizqi Indirsyah Fadhil

---
+++ Rizqi Indirsyah Fadhil_ITB/Rizqi Indirsyah fadhil_ITB.py
@@ -0,0 +1,8 @@
+nama = input("Masukkan  nama Anda : ")
+jurusan = input("Masukkan jurusan Anda : ")
+alasan = input("Masukkan alasan mengapa Anda ingin masuk GDSC : ")
+
+print(f"Nama : {nama}")
+print(f"Jurusan : {jurusan}")
+print(f"Alasan ingin masuk GDSC : {alasan}")
+

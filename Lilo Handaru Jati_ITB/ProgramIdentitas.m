31ebc8b98466be159150190189d35a08f103ff7f
ecstcyy <92445321+ecstcyy@users.noreply.github.com>
2021-10-16 23:38:58 +0700

    Add files via upload

---
+++ Lilo Handaru Jati_ITB/ProgramIdentitas.m
@@ -0,0 +1,6 @@
+% GDSC-Git-Practice
+% Task
+
+fprintf('Nama: Lilo Handaru Jati\n')
+fprintf('Jurusan: FTMD\n')
+fprintf('Alasan ingin masuk GDSC: Ingin belajar lebih banyak mengenai programming dan juga kenalan sama teman baru buat belajar bareng')

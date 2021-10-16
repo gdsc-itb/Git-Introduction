629e40c6c447a37687d4b7af752e3044624ff7b1
frxcksz <92621696+frxcksz@users.noreply.github.com>
2021-10-16 16:33:53 +0700

    First submission by Rangga Kalam Sidiq

---
+++ Rangga Kalam Sidiq_Universitas Pendidikan Indonesia/program.py
@@ -0,0 +1,7 @@
+nama = "Rangga Kalam Sidiq"
+jurusan = "Rekayasa Perangkat Lunak"
+alasan = "Karena saya tertarik dengan bahasan/topik yang berhubungan dengan data science, lalu ingin meningkatkan kemampuan menulis kode program dan mempelajari banyak hal yang mungkin belum dipelajari sebelumnya, juga saya ingin mencari teman dan relasi yang lebih luas dengan orang-orang yang memiliki minat/ketertarikan yang sama."
+
+print("Nama                     : ", nama)
+print("Jurusan                  : ", jurusan)
+print("Alasan ingin masuk GDSC  : ", alasan)

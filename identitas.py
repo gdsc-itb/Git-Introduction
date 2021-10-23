ba45489a3fb8794de6ed5de5266f00fc218ceb8c
Hanif Muhammad Zhafran <16521066@mahasiswa.itb.ac.id>
2021-10-23 14:15:21 +0700

    Adding identitas.py

---
+++ identitas.py
@@ -0,0 +1,6 @@
+nama = 'Hanif Muhammad Zhafran'
+jurusan = 'STEI'
+
+print('Nama: ' + nama)
+print('Jurusan: ' + jurusan)
+print('Alasan ingin masuk GDSC: Ingin mendalami dunia per-IT-an dan menambah relasi')

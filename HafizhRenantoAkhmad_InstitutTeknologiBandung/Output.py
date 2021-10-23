245b72fc8dafa45ab6eb4a41860151306b0a7561
Hafizh <hafizhrenanto88@gmail.com>
2021-10-23 13:40:17 +0700

    First submission by Hafizh

---
+++ HafizhRenantoAkhmad_InstitutTeknologiBandung/Output.py
@@ -0,0 +1,7 @@
+Nama = 'Hafizh Renanto Akhmad'
+Jurusan = 'Fakultas Teknik Mesin dan Dirgantara'
+Alasan = 'Saya ingin belajar tentang data science dan software engineering karena ia adalah minat saya selain yang saya pelajari di jurusan, ingin tahu hal-hal umum tentang produk Google, dan mencari relasi dengan orang-orang baru terutama yang lebih berpengalaman.'
+
+print('Nama : ' + Nama)
+print('Jurusan : ' + Jurusan)
+print('Alasan ingin masuk GDSC : ' + Alasan)

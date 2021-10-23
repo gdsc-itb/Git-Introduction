ec0548b3c94c42ee00e8ed6bbfe033c18f175e53
Hanif Muhammad Zhafran <16521066@mahasiswa.itb.ac.id>
2021-10-23 14:45:19 +0700

    Remove identitas.py

--- identitas.py
+++
@@ -1,6 +0,0 @@
-nama = 'Hanif Muhammad Zhafran'
-jurusan = 'STEI'
-
-print('Nama: ' + nama)
-print('Jurusan: ' + jurusan)
-print('Alasan ingin masuk GDSC: Ingin mendalami dunia per-IT-an dan menambah relasi')

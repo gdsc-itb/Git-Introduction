2cc1b6bccdb40bb8edac2c414ddf0e2b1a22e7b0
Asy Syaffa Khoirunnisa <90151937+asysyaffak@users.noreply.github.com>
2021-10-25 08:46:51 +0700

    Update main.c

--- AsySyaffa_PENS/main.c
+++ AsySyaffa_PENS/main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    printf("Nama : Asy Syaffa\n");
+    printf("Nama : Asy Syaffa Khoirunnisa\n");
     printf("Jurusan : Teknik Informatika\n");
     printf("Alasan ingin masuk GDSC : I think it would be great to be a part of a community that is engaged in a field that I'm interested in i.e. technology.");
     

8ba72f31be3d0c5cba4559ce04954cb6e1c918a5
RaydenF <raydenfairlee@gmail.com>
2022-01-28 19:39:03 +0700

    28 Januari 2022 tinggal style

--- Rayden Fairlee_Institut Pertanian Bogor/frontend'/script.js
+++ Rayden Fairlee_Institut Pertanian Bogor/frontend'/script.js
@@ -1,51 +1,26 @@
-// Get the modal
-var modal = document.getElementById("ModalFilm1");
+const Lists=document.querySelectorAll('[data-target]')
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
 
-// Get the button that opens the modal
-var btn = document.getElementById("List1");
+Lists.forEach((List) => {
+  List.addEventListener("click", () => {
+    document.querySelector(List.dataset.target).classList.add("active");
+    overlay.classList.add("active");
+  });
+});
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close1")[0];
+close_modals.forEach((List) => {
+  List.addEventListener("click", () => {
+    const modal = List.closest(".modal");
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = (event) => {
+  if (event.target == overlay) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => modal.classList.remove("active"));
+    overlay.classList.remove("active");
   }
-}
-
-// Get the modal
-var modal = document.getElementById("ModalFilm2");
-
-// Get the button that opens the modal
-var btn = document.getElementById("List2");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close2")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+};
